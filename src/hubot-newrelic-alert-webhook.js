33bf203ef57c09cc415e9dd800077841738e5867
Adriano Godoy <godoy.ccp@gmail.com>
2016-06-24 15:56:41 -0300

    Adds url post receive

---
+++ src/hubot-newrelic-alert-webhook.js
@@ -0,0 +1,46 @@
+// Description
+//   A hubot script to get google analytics reports
+//
+// Configuration:
+//   NR_ALERT_ROOM - The default room to which message should go (optional)
+//
+// Commands:
+//   None
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// URLS:
+//   POST /hubot/nr-alert-webhook?room=<room>
+//
+// Author:
+//   Adriano Godoy
+
+var inspect, querystring, url, room;
+
+inspect = (require('util')).inspect;
+url = require('url');
+querystring = require('querystring');
+
+module.exports = function(robot) {
+  return robot.router.post("/hubot/nr-alert-webhook", function(req, res) {
+    var data, query, room;
+    query = querystring.parse(url.parse(req.url).query);
+    data = req.body;
+
+    robot.logger.debug("Received New Relic POST: " + (inspect(data)));
+    console.log("Received New Relic POST: " + (inspect(data)));
+
+    room = query.room || process.env["NR_ALERT_ROOM"];
+
+    try {
+      console.log("New relic report: " + (inspect(data)))
+      return robot.messageRoom(room, (inspect(data)));
+    } catch (_error) {
+      error = _error;
+      robot.messageRoom(room, "NR Alert error: " + error);
+      console.log("NR Alert error: " + error + ". Request: " + req.body);
+    }
+    return res.end("");
+  });
+};
